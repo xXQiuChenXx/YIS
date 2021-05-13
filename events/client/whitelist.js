4256f84a5d6539e89fad0130b523964f4a8feb2e
秋辰 <51996408+QC-Development@users.noreply.github.com>
2021-05-13 17:21:12 +0800

    Update whitelist.js

--- events/client/whitelist.js
+++ events/client/whitelist.js
@@ -1,7 +1,7 @@
 const { confirmation } = require("reconlx");
 const Discord = require('discord.js');
 module.exports = async (bot, message, config) => {
-    if (message.channel.id === config.WhitelistConsole) {
+    if (message.channel.id === "835034616044716073") {
         if (message.author.bot) {
             if (message.content.includes('你還沒加白名單')) {
                 let RE = message.content.split("name=")[0]
